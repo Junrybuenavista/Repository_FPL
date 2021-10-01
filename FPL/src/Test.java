b0743845a7ea76c2f59e5712844e916d8e662035
Junrybuenavista <42229618+Junrybuenavista@users.noreply.github.com>
2021-10-01 22:11:13 +0800

    s

--- FPL/src/Test.java
+++ FPL/src/Test.java
@@ -117,8 +117,8 @@ public class Test {
 						driver.get("https://www.fpl.com/my-account/multi-dashboard.html");
 										
 				}
-				testmedddddddddd
-				DL.stop();
+				testd
+				DL.stop(fdsf);
 				
 		
 				
